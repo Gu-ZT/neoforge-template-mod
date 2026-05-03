eae6dc4e09d6398f13e1ec334a38111e5ae8e487
Gugle <administration@ztxy666.cn>
2026-05-03 19:24:12 +0800

    Add initial project setup with Gradle configuration, bug report and feature request templates, and .gitignore

---
+++ src/main/java/dev/dubhe/template/NeoforgeTemplateMod.java
@@ -0,0 +1,13 @@
+package dev.dubhe.template;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(NeoforgeTemplateMod.MOD_ID)
+public class NeoforgeTemplateMod {
+    public static final String MOD_ID = "neoforge_template_mod";
+
+    public NeoforgeTemplateMod(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
